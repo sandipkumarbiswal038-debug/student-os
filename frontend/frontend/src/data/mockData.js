c06ae9e54fb659f10233ac2f20da7193184192c2
saswati <sm9896315@gmail.com>
2026-07-06 14:48:55 -0700

    Added student and faculty attendance pages

---
+++ frontend/frontend/src/data/mockData.js
@@ -0,0 +1,39 @@
+export const studentData = {
+  name: "Subhashree Jena",
+  rollNo: "MCA-25069",
+  semester: "3rd",
+  overallAttendance: "89%",
+};
+export const attendanceData = [
+  {
+    subject: "Datastructures",
+    percentage: "90%",
+  },
+  {
+    subject: "Java",
+    percentage: "85%",
+  },
+  {
+    subject: "Python",
+    percentage: "95%",
+  },
+  {
+    subject: "Operating System",
+    percentage: "88%",
+  },
+  {
+    subject: "DBMS",
+    percentage: "91%",
+  },
+];
+export const facultyData = {
+  subject: "Java",
+  date: "07 July 2026",
+};
+export const classRoll = [
+  { id: 1, name: "Subhashree" },
+  { id: 2, name: "Priya" },
+  { id: 3, name: "Aman" },
+  { id: 4, name: "Riya" },
+  { id: 5, name: "Arjun" },
+];
---
+++ frontend/frontend/src/pages/FacultyAttendance.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { facultyData, classRoll } from "../data/mockData";
+import "../styles/FacultyAttendance.css";
+function FacultyAttendance() {
+    return (
+        <div className="faculty-container">
+                 <h1 className="title">
+            Faculty Attendance
+            </h1>
+            <div className="faculty-card">
+
+    <h2>Class Details</h2>
+
+    <p>
+        <strong>Subject:</strong> {facultyData.subject}
+    </p>
+
+    <p>
+        <strong>Date:</strong> {facultyData.date}
+    </p>
+
+</div>
+<div className="attendance-list">
+
+    <h2>Mark Attendance</h2>
+
+    <table>
+
+        <thead>
+
+            <tr>
+
+                <th>Student Name</th>
+
+                <th>Present</th>
+
+                <th>Absent</th>
+
+            </tr>
+
+        </thead>
+
+        <tbody>
+
+            {classRoll.map((student) => (
+
+                <tr key={student.id}>
+
+                    <td>{student.name}</td>
+
+                    <td>
+                        <input type="checkbox" />
+                    </td>
+
+                    <td>
+                        <input type="checkbox" />
+                    </td>
+
+                </tr>
+
+            ))}
+
+        </tbody>
+
+    </table>
+            <div className="button-container">
+    <button className="save-btn">
+        Save Attendance
+    </button>
+</div>
+
+</div>
+
+        </div>
+    );
+}
+export default  FacultyAttendance;
