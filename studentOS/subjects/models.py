4a79d9b716355929396c5be7e75de8ffd25f34ae
Saninee Priyadarshini <sanineepriyadarshini046@gmail.com>
2026-07-28 07:14:28 +0530

    Added course_code and course_name to suject model

--- studentOS/subjects/models.py
+++ studentOS/subjects/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Subject(models.Model):
   
-    
+    course_code = models.CharField(max_length=20, unique=True)
+    course_name = models.CharField(max_length=100, unique=True)
     subject_code = models.CharField(max_length=20, unique=True)
     subject_name = models.CharField(max_length=100, unique=True)
     semester = models.PositiveIntegerField()
@@ -11,5 +12,3 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.subject_name
-
-
