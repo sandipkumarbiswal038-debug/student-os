de58d6ba42596563ea4895ff0dc4fbe9698c6f94
Saninee Priyadarshini <sanineepriyadarshini046@gmail.com>
2026-07-30 21:33:41 +0530

    added settings.py attendance url

--- studentOS/studentOS/settings.py
+++ studentOS/studentOS/settings.py
@@ -192,11 +192,17 @@ ACCOUNT_USERNAME_REQUIRED = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
