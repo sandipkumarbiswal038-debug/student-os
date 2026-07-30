3b20ffa3a214789514c58263536a3c54092b5fe9
Saninee Priyadarshini <sanineepriyadarshini046@gmail.com>
2026-07-30 10:50:21 +0530

    Add new attribute in classes

--- studentOS/classes/models.py
+++ studentOS/classes/models.py
@@ -4,11 +4,12 @@ from subjects.models import Subject
 
 class ClassSession(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    course = models.CharField(max_length=100)
+    section = models.CharField(max_length=20)
+    semester = models.PositiveIntegerField()
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
 
     def __str__(self):
         return f"{self.subject.subject_name} - {self.date}"
-
-
--- studentOS/classes/serializers.py
+++ studentOS/classes/serializers.py
@@ -2,14 +2,15 @@ from rest_framework import serializers
 from .models import ClassSession
 
 class ClassSessionSerializer(serializers.ModelSerializer):
-    subject_name = serializers.CharField(source="subject.subject_name", read_only=True)
 
     class Meta:
         model = ClassSession
         fields = [
             "id",
             "subject",
-            "subject_name",
+            "course",
+            "section",
+            "semester",
             "date",
             "start_time",
             "end_time",
