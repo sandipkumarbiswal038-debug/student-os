c72bcca69f5a25ed722d04a090c5af67ce8244f6
Saninee Priyadarshini <sanineepriyadarshini046@gmail.com>
2026-07-31 11:38:46 +0530

    add html part for design

--- studentOS/studentOS/settings.py
+++ studentOS/studentOS/settings.py
@@ -158,8 +158,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
+
+
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = "/"
